82007dfef243f9ad6135ced3a897a22be4fe5383
Paul Rosania <paul.rosania@gmail.com>
2010-01-21 10:43:32 -0500

    Removed pass-through method. Now, when popups fail to open, they can be managed/allowed by the user.

--- hacker_sync/content_script.js
+++ hacker_sync/content_script.js
@@ -4,22 +4,20 @@
       var link = $(this).closest('tr').find('td.title a');
       var href = link.attr('href');
       var title = link.text();
-      
+
       if (!href.match(/^https?:\/\//)) {
         href = "http://news.ycombinator.com/" + href;
       }
-      
+
       var save_url = 'http://delicious.com/save?url='
                    + encodeURIComponent(href)
                    + '&title='
                    + encodeURIComponent(title)
                    + '&v=5&';
 
-      if (!window.open(save_url + 'noui=1&jump=doclose',
-                       'deliciousuiv5',
-                       'location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550')) {
-        location.href = save_url + 'jump=yes';
-      }
+      window.open(save_url + 'noui=1&jump=doclose',
+                  'deliciousuiv5',
+                  'location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
     });
   });
 })(jQuery);
