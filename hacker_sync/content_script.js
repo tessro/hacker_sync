463fe0758ff8bd23feb90692e816fc028e791c34
Paul Rosania <paul.rosania@gmail.com>
2010-02-12 14:48:51 -0500

    Delicious icon no longer appears for comments

--- hacker_sync/content_script.js
+++ hacker_sync/content_script.js
@@ -1,31 +1,33 @@
 (function($) {
   $(document).ready(function() {
     $('td a[href^="vote"]').click(function(e) {
-      $(this).unbind('click');
-      $(this).hide();
-      
-      container = $(this).closest('center');
-      container.append('<a class="delicious" href="javascript:void(null);"><img src="' + chrome.extension.getURL('assets/delicious_10.png') + '" /></a>')
-      
+      var container = $(this).closest('center');
+      var link = container.closest('tr').find('td.title a');
+
+      if (link.length == 0) return;
+
+      container.html('<a class="delicious" href="javascript:void(null);"><img src="' + chrome.extension.getURL('assets/delicious_10.png') + '" /></a>');
+
       container.find('a.delicious').click(function(e) {
         e.preventDefault();
-        var link = $(this).closest('tr').find('td.title a');
+
         var href = link.attr('href');
         var title = link.text();
+
         if (!href.match(/^https?:\/\//)) {
           href = "http://news.ycombinator.com/" + href;
         }
-      
+
         var save_url = 'http://delicious.com/save?url='
                      + encodeURIComponent(href)
                      + '&title='
                      + encodeURIComponent(title)
                      + '&v=5&';
-      
+
         window.open(save_url + 'noui=1&jump=doclose',
                     'deliciousuiv5',
                     'location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
-	      container.text('<img src="' + chrome.extension.getURL('assets/checkmark_10.png') + '" alt="" />');
+        container.html('<img src="' + chrome.extension.getURL('assets/checkmark_10.png') + '" alt="" />');
       });
     });
   });
